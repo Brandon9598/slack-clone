43b2df09e48d569dfac4f862a86121c3e0b58e2b
brandon9598 <brandon9598@gmail.com>
2020-12-24 02:56:35 -0500

    styled chat functionallity

--- src/ChatInput.js
+++ src/ChatInput.js
@@ -24,6 +24,8 @@ function ChatInput({ channelName, channelId }) {
             "https://secure.gravatar.com/avatar/e3b51ca72dee4ef87916ae2b9240df50.jpg?s=512&d=https%3A%2F%2Fdev.slack.com%2Fimg%2Favatars%2Fava_0010-512.v1443724322.png",
         });
     }
+
+    setInput("");
   };
   return (
     <div className="chatInput">
